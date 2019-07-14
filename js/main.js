ea458cb82d0b005333695cb33565620f639dfce5
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-14 16:29:02 +0300

    Did refactoring. Created array with two figures for tetris and created functions which creating random number, creating new figures cells and adds them class

--- js/main.js
+++ js/main.js
@@ -1,40 +1,85 @@
-
-    // определяю в переменную елемент страницы
+window.onload = function () {
     let main = document.getElementById('main');
     let excel = document.getElementsByClassName('excel');
 
-    //вспомогательная переменная
-    let i = 0;
-
-    //создаю элементы
     let tetris = document.createElement('div');
 
-    //добавляю классы
-    tetris.classList.add('tetris');
+    //массив с фигурками для игры в тетрис
+    let mainArr = [
+        //палка
+        [
+            [0, 1],
+            [0, 2],
+            [0, 3]
+        ],
+        //квадрат
+        [
+            [1, 0],
+            [0, 1],
+            [1, 1]
+        ]
+    ];
+    //пустой массив рандомных отображаемых фигурок
+    let figureBody = [];
 
-    // в елемент "тетрис" добавляю елементы "эксель"
-    let addExelElements = (a = tetris) => {
+    let addClassToElements = (elem, className) => elem.classList.add(className);
+    let appendElements = (parentElem, childElem) => parentElem.appendChild(childElem);
+    let appendExelElements = (parentElem = tetris) => {
         for (let i = 180; i > 0; i--) {
             let excel = document.createElement('div');
-            excel.classList.add('excel');
-            a.appendChild(excel);
+            addClassToElements(excel, 'excel');
+            parentElem.appendChild(excel);
         }
     };
-    addExelElements();
-
-    main.appendChild(tetris);
-
-    //каждой ячейке присваиваю позицию по осям X, Y
-    let addArrtExcel = () => {
+    let addPositionArrtToElem = (elem = excel) => {
+        let i = 0;
         for (let y = 18; y > 0; y--) {
             for (let x = 1; x < 11; x++) {
-                excel[i].setAttribute('posX', x);
-                excel[i].setAttribute('posY', y);
+                elem[i].setAttribute('posX', x);
+                elem[i].setAttribute('posY', y);
                 i++;
             }
         }
     }
-    addArrtExcel();
+
+
+    //функция рандомного значения
+    let getRandom = (array) => Math.round(Math.random() * (array.length - 1));
+    //функция для создания фигурок для тетриса, принимает стартовые значения по оси X и Y
+    let createFigureForTetris = (x, y) => {
+        // локальная переменная с рандомным числом
+        let currentFigure = getRandom(mainArr);
+        //наполняю массив в котором храняться рандомные фигурки
+        figureBody = [
+            document.querySelector(`[posX = "${x}"][posY = "${y}"]`),  //координаты для первой ячейки фигурки
+            document.querySelector(`[posX = "${x + mainArr[currentFigure][0][0]}"][posY = "${y + mainArr[currentFigure][0][1]}"]`),
+            document.querySelector(`[posX = "${x + mainArr[currentFigure][1][0]}"][posY = "${y + mainArr[currentFigure][1][1]}"]`),
+            document.querySelector(`[posX = "${x + mainArr[currentFigure][2][0]}"][posY = "${y + mainArr[currentFigure][2][1]}"]`),
+        ]
+    };
+    //присваиваю класс для каждой ячейки созданной фигуроки
+    let addFigureClass = (figure = figureBody) => {
+        for (let i = 0; i < figure.length; i++) {
+            figure[i].classList.add('figure');
+        }
+    }
+
+
+
+
+
+
+
+
+
+    addClassToElements(tetris, 'tetris');
+    appendElements(main, tetris);
+    appendExelElements();
+    addPositionArrtToElem();
+    createFigureForTetris(5, 10);
+    addFigureClass();
+
+};
 
 
 
